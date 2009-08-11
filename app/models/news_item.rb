c24a456c5720d3d86cd304b85b88845cbcfdb7b9
Roman Smirnov <POMAHC@gmail.com>
2009-08-11 13:15:50 +0400

    Add method to retrieve last news

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -7,4 +7,16 @@ class NewsItem < ActiveRecord::Base
     NewsItem.find(:first,:conditions=>["identifier=? and published_start_at<?",identifier,Time.now],:order=>" RAND()")
   end
   
+  def self.last_news(identifier, limit = false)
+    if limit
+      NewsItem.all(:conditions=>
+      ["identifier=? AND (published_start_at<? OR published_start_at IS NULL) AND (published_end_at>? OR published_end_at IS NULL)",
+      identifier, Time.now, Time.now], :order=>"created_at DESC", :limit => limit)
+    else
+      NewsItem.all(:conditions=>
+      ["identifier=? AND (published_start_at<? OR published_start_at IS NULL) AND (published_end_at>? OR published_end_at IS NULL)",
+      identifier, Time.now, Time.now], :order=>"created_at DESC")
+    end
+  end
+  
 end
